0b709c05e2dc6ee3b7fd1dbfd9b942b20ff053be
Priyanka Palkar <palkar_p@India.XoriantCorp.com>
2024-05-06 13:34:15 +0530

    first commit for jenkin app

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello..!!");
+        System.out.println("Jenkins App..");
+    }
+}
